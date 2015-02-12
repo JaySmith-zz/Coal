d22355f18a0aef795c0b9b559f7411bdf784b763
JaySmith <jay@jaysmith.us>
2015-02-12 13:51:59 -0600

    initial commit

---
+++ Source/Coal/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coal.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Modified { get; set; }
+        public DateTime ModifiedBy { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime CreatedBy { get; set; }
+    }
+}
---
+++ Source/Coal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Coal.Startup))]
+namespace Coal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
